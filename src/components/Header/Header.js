22a8b30c4070e4283c97dd87ec82355957881b69
JaRED-17 <melnikow_igor@mail.ru>
2021-01-07 14:32:06 +0600

    Added classes and sass

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.scss';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
