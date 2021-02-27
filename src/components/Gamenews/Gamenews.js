02ddd323566e078d7da13c0b83c98af218348b41
JaRED-17 <melnikow_igor@mail.ru>
2021-02-27 21:08:48 +0600

    draft for fetching html data

--- src/components/Gamenews/Gamenews.js
+++ src/components/Gamenews/Gamenews.js
@@ -2,10 +2,39 @@ import React from "react";
 
 class Gamenews extends React.Component {
 
-  render() {
+  state = {
+    isLoading: true,
+    content: ''
+  };
+
+  get news() {
+    fetch('../../media/news/15.02.html').then((response) => {
+      return response.text();
+    }).then((html) => {
+      return this.setState({
+        isLoading: false,
+        content: html
+      });
+    });
+  }
+
+  get content() {
+    return <div className="Gamenews"></div>
+  }
+
+  get newsContent() {
     return (
-        <div className="Gamenews"></div>
-    );
+        <div className="Gamenews">
+          {this.state.content}
+        </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state;
+    // eslint-disable-next-line no-unused-expressions
+    isLoading ? this.news : null;
+    return isLoading ? this.content : this.newsContent;
   }
 }
 
