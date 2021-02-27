1a58325c676ef0d9756530b233977880e2b5c78e
JaRED-17 <melnikow_igor@mail.ru>
2021-02-27 19:13:18 +0600

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]-[hash]-bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080,
+        watchContentBase: true,
+        progress: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[hash].css'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/media'),
+                    to: path.resolve(__dirname, 'dist/media')
+                }
+            ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    }
+};
