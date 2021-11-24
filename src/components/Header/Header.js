bd068e6ba0841233e6142d7fd82bb1380eb2b33e
Igor Borozdov <melnikow_igor@mail.ru>
2021-11-24 22:13:06 +0600

    Fixed routing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,28 @@
 import React from "react";
 import Button from "../Button";
-import { createBrowserHistory } from 'history';
+import { withRouter } from "react-router-dom";
 import PopupStore from "../../store";
-
-const history = createBrowserHistory();
 
 class Header extends React.Component {
 
-    clickHandler = (path, isPopupOpen) => {
-        isPopupOpen ? PopupStore.openPopup() : PopupStore.closePopup();
-        history.push(`/${path}`);
+    clickHandler = (path) => {
+        PopupStore.openPopup()
+        this.props.history.push(`/${path}`);
     }
 
     render () {
-        const { isPopupOpen } = PopupStore;
-
         return (
             <div className="Header">
                 <div className="Logo">
                       <h1>Game news</h1>
                 </div>
                 <div className="Buttons">
-                    <Button className="primary" name="Login" handleClick={() => this.clickHandler('login', isPopupOpen)} />
-                    <Button className="secondary" name="Registration" handleClick={() => this.clickHandler('register', isPopupOpen)} />
+                    <Button className="primary" name="Login" handleClick={() => this.clickHandler('login')} />
+                    <Button className="secondary" name="Registration" handleClick={() => this.clickHandler('register')} />
                 </div>
             </div>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
