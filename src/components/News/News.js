1779ab3dc254270bd2761c2c2a5fb415be673b14
Igor Borozdov <igor.borozdov@finnplay.com>
2021-11-26 11:18:01 +0600

    Changed orientation for news

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,7 +3,7 @@ import React from "react";
 class News extends React.Component {
     render () {
         return (
-            <div className="News col s6" dangerouslySetInnerHTML={{ __html: this.props.content }} />
+            <div className="News col s12" dangerouslySetInnerHTML={{ __html: this.props.content }} />
         )
     }
 }
