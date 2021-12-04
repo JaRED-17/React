4ff2000779fdacd387f81f65a7a93062f27f19b7
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-04 20:54:14 +0600

    Code refactoring

--- src/core/News/News.js
+++ src/core/News/News.js
@@ -2,24 +2,24 @@ const defaultValue = { date: '15.02.2021', html: '' };
 
 class News {
 
-    static news = (date) => {
-        return fetch('/api/news/onenews', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify({'date': date || defaultValue.date})
-        })
+    static fetchData = (url, init, defaultValue) => {
+        return fetch(url, init)
             .then(response => response.status === 200 ? response.json() : defaultValue)
             .then(response => response)
             .catch(error => console.error(error.message));
     }
 
-    static allNews = () => fetch('/api/news/allnews')
-        .then(response => response.status === 200 ? response.json() : [defaultValue])
-        .then(response => response)
-        .catch(error => console.error(error.message));
+    static news = (date) => {
+        return this.fetchData('/api/news/onenews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({'date': date || defaultValue.date})
+        }, defaultValue);
+    }
 
+    static allNews = () => this.fetchData('/api/news/allnews', null, [defaultValue]);
 }
 
 export default News;
