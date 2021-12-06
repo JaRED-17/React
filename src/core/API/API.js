5e74810ed8e913ff143adee7ccac10576bb01bc4
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-06 20:00:28 +0600

    Common API class draft

--- src/core/API/API.js
+++ src/core/API/API.js
@@ -1,16 +1,41 @@
 class API {
 
+    static loading = false;
+    static error = false;
+    static errorMessage = '';
+
+    /* loading */
+    static getLoading = () => this.loading;
+    static setLoading = flag => this.loading = flag;
+
+    /* error */
+    static getError = () => this.error;
+    static setError = flag => this.error = flag;
+
+    /* errorMessage */
+    static getErrorMessage = () => this.errorMessage;
+    static setErrorMessage = message => this.errorMessage = message;
+
     static callAPI = (url, init = null, defaultValue = '', type = 'json') => {
+        this.setLoading(true);
+        this.setError(false);
+        this.setErrorMessage('');
         return fetch(url, init)
             .then(response => {
                 return response.status === 200
-                    ? type === 'json'
-                        ? response.json()
-                        : response.text()
+                    ? response[type]()
                     : defaultValue
             })
-            .then(response => response)
-            .catch(error => console.error(error.message));
+            .then(response => {
+                this.setLoading(false);
+                return response;
+            })
+            .catch(error => {
+                this.setLoading(false);
+                this.setError(true);
+                this.setErrorMessage(error.message);
+                console.error(error.message);
+            });
     }
 }
 
