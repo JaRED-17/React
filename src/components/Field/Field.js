32e7718995f80bf096333db2d782222ff7687926
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-12 11:38:26 +0600

    Added styles for Field component

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -6,14 +6,12 @@ class Field extends React.Component {
     render () {
         const { type, name, hidden } = this.props
 
-        console.log(translations[name]);
-
         return (
-            <>
+            <div className={`Field ${name}`}>
                 <label>{translations[name]}</label>
                 <input type={type} name={name} ref={name} />
                 <div className={`warning ${hidden}`}>{translations[name]} must be filled in</div>
-            </>
+            </div>
         )
     }
 }
