86f1d3b466cff4b894a9002e51fd1392776e91fb
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-12 12:32:08 +0600

    Send correct data for registration

--- src/forms/RegistrationForm/RegistrationForm.js
+++ src/forms/RegistrationForm/RegistrationForm.js
@@ -1,28 +1,37 @@
 import React from "react";
 import Button from "../../components/Button";
 import core from "../../core/Core";
+import Field from "../../components/Field"
 import { withRouter } from "react-router-dom";
 
 class RegistrationForm extends React.Component {
 
     login = () => this.props.history.push('/login');
 
-    registration = () => core.user.registration.API({login: 'user', password: 'password'}).then(response => console.log(response))
+    registration = (data) => core.user.registration.API(data).then(response => console.log(response))
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        this.registration({email, password});
+    }
 
     render () {
         return (
             <div className='RegistrationForm'>
                 <h3>Registration</h3>
-                <div className="Form">
-                    <label>Login</label>
-                    <input type="text"/>
-                    <label>Password</label>
-                    <input type="password"/>
-                </div>
+                <form onSubmit={this.onSubmit}>
+                    <div className="Form">
+                        <Field type={'text'} name={'email'} hidden={'hidden'} />
+                        <Field type={'password'} name={'password'} hidden={'hidden'} />
+                    </div>
 
-                <div className="Buttons">
-                    <Button className="secondary" name="Registration" onClick={this.registration} />
-                </div>
+                    <div className="Buttons">
+                        <Button type="submit" className="secondary" name="Registration" />
+                    </div>
+                </form>
 
                 <hr/>
 
