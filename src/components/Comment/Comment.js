7fcc6cb676be03060336e8ca572dab9c2202cc6e
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-13 22:55:32 +0600

    Added header for comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -7,10 +7,17 @@ class Comment extends React.Component {
 
         return (
             <div className='Comment'>
+                <div className='Comment-header'>
+                    <div className='avatar'></div>
+                    <div>
+                        <div className='userName'>User Name</div>
+                        <div className='date'>01.01.2021</div>
+                    </div>
+                </div>
                 <div className='Comment-content'>
                     {content}
                 </div>
-                <div className='Answers'>
+                <div className='Comment-answers'>
                     <p>Ответов: {answers}</p>
                     <a href='#'>Ответить</a>
                 </div>
