b02e8189574973a9b3c608b3cb11c4931110861e
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-14 21:10:08 +0600

    Added go back button

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Button from "../../components/Button";
+import { withRouter } from "react-router-dom";
 
 class NotFoundPage extends React.Component {
+
+    back = () => this.props.history.push('/');
 
     render () {
         return (
             <div className='NotFoundPage'>
                 <h3>Page not found</h3>
+                <div className="Buttons">
+                    <Button className="primary" name="back" onClick={this.back} />
+                </div>
             </div>
         )
     }
 }
 
-export default NotFoundPage;
+export default withRouter(NotFoundPage);
