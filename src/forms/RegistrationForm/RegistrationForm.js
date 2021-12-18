79ac429fad3004a732d394a86d0b97bddac6561a
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-18 15:36:37 +0600

    Added validation for Registration Form

--- src/forms/RegistrationForm/RegistrationForm.js
+++ src/forms/RegistrationForm/RegistrationForm.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Button from "../../components/Button";
+import Validation from "../../validation";
 import core from "../../core/Core";
 import Field from "../../components/Field"
 import { withRouter } from "react-router-dom";
 import translations from "./translations";
 
 class RegistrationForm extends React.Component {
+
+    state = {
+        emailHasError: false,
+        passwordHasError: false,
+    }
 
     login = () => this.props.history.push('/login');
 
@@ -15,11 +21,23 @@ class RegistrationForm extends React.Component {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
+        const emailValidation = Validation.email(email).success;
+        const passwordValidation = Validation.password(password).success;
+
+        this.setState({emailHasError: !emailValidation});
+        this.setState({passwordHasError: !passwordValidation});
+
+        if (!emailValidation || !passwordValidation) {
+            return;
+        }
 
         this.registration({email, password});
     }
 
     render () {
+        const { emailHasError, passwordHasError } = this.state;
+        const hiddenEmail = emailHasError ? '' : 'hidden';
+        const hiddenPassword = passwordHasError ? '' : 'hidden';
         const language = 'ru'
 
         return (
@@ -27,8 +45,8 @@ class RegistrationForm extends React.Component {
                 <h3>{translations[language]['app.form.registration.title']}</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="Form">
-                        <Field type={'text'} name={'email'} hidden={'hidden'} />
-                        <Field type={'password'} name={'password'} hidden={'hidden'} />
+                        <Field type={'text'} name={'email'} hidden={hiddenEmail} />
+                        <Field type={'password'} name={'password'} hidden={hiddenPassword} />
                     </div>
 
                     <div className="Buttons">
