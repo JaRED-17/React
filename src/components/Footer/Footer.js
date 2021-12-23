d154ab13c42776dadeeebbec73d40a4277c8cfdc
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-23 19:57:19 +0600

    Added translations for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import translations from './translations'
+import Translations from '../../translations'
 
 class Footer extends React.Component {
+    messages = new Translations(translations, 'app.footer.')
+
     render () {
         return (
             <footer className='page-footer'>
                 <div className='container'>
                     <div className='row'>
                         <div className='col l6 s12'>
-                            <h5 className='white-text'>Footer Content</h5>
-                            <p className='grey-text text-lighten-4'>We are your salvation!!!</p>
+                            <h5 className='white-text'>{this.messages.getTranslations('header.title')}</h5>
+                            <p className='grey-text text-lighten-4'>{this.messages.getTranslations('header.content')}</p>
                         </div>
                         <div className='col l4 offset-l2 s12'>
                             <h5 className='white-text'>Links</h5>
                             <ul>
-                                <li><Link className='grey-text text-lighten-3' to={'/'}>Home</Link></li>
-                                <li><Link className='grey-text text-lighten-3' to={'/news'}>News</Link></li>
-                                <li><Link className='grey-text text-lighten-3' to={'/reviews'}>Reviews</Link></li>
-                                <li><Link className='grey-text text-lighten-3' to={'/articles'}>Articles</Link></li>
+                                <li><Link className='grey-text text-lighten-3' to={'/'}>{this.messages.getTranslations('link.home')}</Link></li>
+                                <li><Link className='grey-text text-lighten-3' to={'/news'}>{this.messages.getTranslations('link.news')}</Link></li>
+                                <li><Link className='grey-text text-lighten-3' to={'/reviews'}>{this.messages.getTranslations('link.reviews')}</Link></li>
+                                <li><Link className='grey-text text-lighten-3' to={'/articles'}>{this.messages.getTranslations('link.articles')}</Link></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div className='footer-copyright'>
                     <div className='container'>
-                        © 2014 Copyright Text
-                        <a className='grey-text text-lighten-4 right' href='#!'>More Links</a>
+                        {this.messages.getTranslations('rights')}
+                        <a className='grey-text text-lighten-4 right' href='#!'>{this.messages.getTranslations('link.more')}</a>
                     </div>
                 </div>
             </footer>
---
+++ src/components/Footer/translations.js
@@ -0,0 +1,24 @@
+const translations = {
+    en: {
+        'app.footer.header.title': 'Footer Content',
+        'app.footer.header.content': 'We are your salvation!!!',
+        'app.footer.link.home': 'Home',
+        'app.footer.link.news': 'News',
+        'app.footer.link.reviews': 'Reviews',
+        'app.footer.link.articles': 'Articles',
+        'app.footer.link.more': 'More Links',
+        'app.footer.rights': '© 2021 Copyright Text'
+    },
+    ru: {
+        'app.footer.header.title': 'Контент футера',
+        'app.footer.header.content': 'Мы ваше спасение!!!',
+        'app.footer.link.home': 'Домой',
+        'app.footer.link.news': 'Новости',
+        'app.footer.link.reviews': 'Обзоры',
+        'app.footer.link.articles': 'Статьи',
+        'app.footer.link.more': 'Больше ссылок',
+        'app.footer.rights': '© 2021 Текст об авторских правах'
+    }
+}
+
+export default translations
