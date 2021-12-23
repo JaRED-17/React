41f08c26b763ff6643eb95a18b5077a9fcad636a
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-23 22:16:59 +0600

    Do not show login/registration pages if user is not logged in

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import core from '../../core/Core/Core'
 
 /* Pages */
 import HomePage from '../../pages/HomePage'
@@ -9,12 +10,18 @@ import GameNewsPage from '../../pages/GameNewsPage'
 import NotFoundPage from '../../pages/NotFoundPage'
 
 class Routes extends React.Component {
+    isLoggedIn = () => core.user.isLoggedIn()
+
     render () {
         return (
             <Switch>
                 <Route path='/' component={HomePage} exact />
-                <Route path='/login' component={LoginPage} exact />
-                <Route path='/registration' component={RegistrationPage} exact />
+                {!this.isLoggedIn() ? (
+                    <>
+                        <Route path='/login' component={LoginPage} exact />
+                        <Route path='/registration' component={RegistrationPage} exact />
+                    </>
+                ) : null}
                 <Route path='/news' component={GameNewsPage} exact />
                 <Route component={NotFoundPage} />
             </Switch>
