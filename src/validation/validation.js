04676e1aa0d17f4ce49a495117ddcbea1df94024
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-25 23:55:43 +0600

    Code refactoring

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,5 +1,5 @@
 class Validation {
-    static checkEmpty = (field) => !field || field === ''
+    static isEmpty = (field) => !field || field === ''
 
     static emptyError = () => {
         return {
@@ -9,13 +9,13 @@ class Validation {
         }
     }
 
-    static email = (email) => this.checkEmpty(email) ? this.emptyError() : { success: true }
+    static email = (email) => this.isEmpty(email) ? this.emptyError() : { success: true }
 
-    static password = (password) => this.checkEmpty(password) ? this.emptyError() : { success: true }
+    static password = (password) => this.isEmpty(password) ? this.emptyError() : { success: true }
 
-    static firstName = (firstName) => this.checkEmpty(firstName) ? this.emptyError() : { success: true }
+    static firstName = (firstName) => this.isEmpty(firstName) ? this.emptyError() : { success: true }
 
-    static lastName = (lastName) => this.checkEmpty(lastName) ? this.emptyError() : { success: true }
+    static lastName = (lastName) => this.isEmpty(lastName) ? this.emptyError() : { success: true }
 }
 
 export default Validation
