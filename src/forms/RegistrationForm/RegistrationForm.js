07ac51a2362644091f67c8bb0870c51120059b77
Igor Borozdov <melnikow_igor@mail.ru>
2021-12-31 11:34:02 +0600

    Code refactoring of RegistrationForm

--- src/forms/RegistrationForm/RegistrationForm.js
+++ src/forms/RegistrationForm/RegistrationForm.js
@@ -1,51 +1,25 @@
 import React from 'react'
 import core from '../../core/Core'
-import Button from '../../components/Button'
-import Validation from '../../validation'
-import Field from '../../components/Field'
+import Form from '../Form'
 
 class RegistrationForm extends React.Component {
     state = {
-        emailHasError: false,
-        passwordHasError: false
+        hasError: false
     }
 
-    registration = (data) => core.user.registration.API(data).then(response => console.log(response))
-
-    onSubmit = (event) => {
-        event.preventDefault()
-        const email = event.target.email.value
-        const password = event.target.password.value
-        const emailValidation = Validation.email(email).success
-        const passwordValidation = Validation.password(password).success
-
-        this.setState({emailHasError: !emailValidation})
-        this.setState({passwordHasError: !passwordValidation})
-
-        if (!emailValidation || !passwordValidation) {
-            return
-        }
-
-        this.registration({email, password})
-    }
+    registration = (data) => core.user.registration.API(data).then(response => {
+        console.log(response)
+        this.setState({hasError: !response.success})
+    })
 
     render () {
-        const { emailHasError, passwordHasError } = this.state
+        const { hasError } = this.state
+        const fields = [
+            {id: 1, type: 'text', name: 'email'},
+            {id: 2, type: 'password', name: 'password'}
+        ]
 
-        return (
-            <div className='RegistrationForm'>
-                <form onSubmit={this.onSubmit}>
-                    <div className='Form'>
-                        <Field type={'text'} name={'email'} showWarning={emailHasError} />
-                        <Field type={'password'} name={'password'} showWarning={passwordHasError} />
-                    </div>
-
-                    <div className='Buttons'>
-                        <Button type='submit' className='secondary' name='registration' />
-                    </div>
-                </form>
-            </div>
-        )
+        return <Form fields={fields} name='registration' formClassName='RegistrationForm' api={this.registration} hasError={hasError} />
     }
 }
 
