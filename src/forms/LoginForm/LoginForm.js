feabc2bda4bbaeeaec066ccfa624d68b9874a6d3
Igor Borozdov <melnikow_igor@mail.ru>
2022-06-04 23:25:35 +0600

    Added settings for fields list

--- src/forms/LoginForm/LoginForm.js
+++ src/forms/LoginForm/LoginForm.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import withCore from '../../helpers/withCore'
 import Form from '../Form'
+import Loading from '../../components/Loading'
+import { observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 
+@observer
 class LoginForm extends React.Component {
     static propTypes = {
         core: PropTypes.object
@@ -11,9 +14,18 @@ class LoginForm extends React.Component {
         hasError: false
     }
 
+    componentDidMount () {
+        this.settings()
+    }
+
     get coreUser () {
         const { core } = this.props
         return core.user
+    }
+
+    get coreSettings () {
+        const { core } = this.props
+        return core.settings.settings
     }
 
     login = (data) => this.coreUser.login.API(data).then(response => {
@@ -26,14 +38,19 @@ class LoginForm extends React.Component {
         this.setState({hasError: !response.success})
     })
 
+    settings = () => this.coreSettings.API()
+
+    get fields () {
+        const response = this.coreSettings.response()
+        return response ? response.forms?.login?.fields : {}
+    }
+
     render () {
         const { hasError } = this.state
-        const fields = [
-            {id: 1, type: 'text', name: 'email'},
-            {id: 2, type: 'password', name: 'password'}
-        ]
+        const fields = this.fields
+        const loading = this.coreSettings.loading()
 
-        return <Form fields={fields} name='login' formClassName='LoginForm' api={this.login} hasError={hasError} />
+        return loading ? <Loading /> : <Form fields={fields} name='login' formClassName='LoginForm' api={this.login} hasError={hasError} />
     }
 }
 
